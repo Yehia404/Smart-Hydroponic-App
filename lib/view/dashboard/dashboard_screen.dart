7e3ee845734e5cd2ec6579ff103c2da9618ef578
ahmedaymanali03 <21p0002@eng.asu.edu.eg>
2025-10-19 21:52:49 +0300

    initial dashboard screen with not working nav bar until rest of views are implemented

--- lib/view/dashboard/dashboard_screen.dart
+++ lib/view/dashboard/dashboard_screen.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  // Placeholder widgets for now
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Dashboard')),
+
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: const Center(
-        child: Text(
-          'Welcome to the Dashboard!',
-          style: TextStyle(fontSize: 24),
-        ),
+      appBar: AppBar(
+        title: const Text('SMART Hydroponic'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_outlined),
+            onPressed: () {},
+
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.dashboard_outlined),
+            label: 'Dashboard',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.sensors),
+            label: 'Sensors',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.toggle_on_outlined),
+            label: 'Controls',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.analytics_outlined),
+            label: 'Analytics',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        onTap: (_) {},// Till all items are implemented
       ),
     );
   }
