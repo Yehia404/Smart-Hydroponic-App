9f1df4dac80de697c8654b2fc849f61e67be035c
Youssef Kassab <21p0010@eng.asu.edu.eg>
2025-10-24 15:18:35 +0300

    notif screen

---
+++ lib/view/dashboard/alerts_notifications_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// NEW: Converted to StatefulWidget to manage filter state
+class AlertsNotificationsScreen extends StatefulWidget {
+  const AlertsNotificationsScreen({super.key});
+
+  @override
+  State<AlertsNotificationsScreen> createState() =>
+      _AlertsNotificationsScreenState();
+}
