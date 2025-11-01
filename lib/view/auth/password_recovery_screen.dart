d56f03e252c63e6639dc181967c541d600248f1e
hanna <hanabakeer24@gmail.com>
2025-11-01 22:36:49 +0200

    Create password recovery screen

---
+++ lib/view/auth/password_recovery_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PasswordRecoveryScreen extends StatelessWidget {
+  const PasswordRecoveryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reset Password'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Enter your email to receive a password reset link.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
