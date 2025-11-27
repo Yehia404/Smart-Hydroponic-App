7a5938aca5f1014315cf0c1a8815c38534a8aba6
ahmedaymanali03 <21p0002@eng.asu.edu.eg>
2025-11-27 13:41:10 +0200

    created folder layout for project

--- lib/view/dashboard/sensor_monitoring_screen.dart
+++ lib/view/dashboard/sensor_monitoring_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../models/sensor_data.dart';
+import '../../data/models/sensor_data.dart';
 
 class SensorMonitoringScreen extends StatelessWidget {
   const SensorMonitoringScreen({super.key});
