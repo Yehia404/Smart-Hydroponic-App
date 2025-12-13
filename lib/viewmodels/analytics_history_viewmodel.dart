2ab801026ed53dd031eeaad1fb8d6486512d724b
Hanna Mohamed <hanabakeer24@gmail.com>
2025-12-14 01:25:07 +0200

    state variables and getters for sensor data done

--- lib/viewmodels/analytics_history_viewmodel.dart
+++ lib/viewmodels/analytics_history_viewmodel.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 import '../data/services/firestore_service.dart';
 
-
 class AnalyticsHistoryViewModel extends ChangeNotifier {
+  BuildContext? _context;
+  bool _isLoading = false;
+  String? _errorMessage;
   
+  // Data storage for each sensor type
+  List<DataPoint> _temperatureData = [];
+  List<DataPoint> _phData = [];
+  List<DataPoint> _waterLevelData = [];
+  List<DataPoint> _lightIntensityData = [];
+  List<DataPoint> _tdsData = [];
+  List<DataPoint> _humidityData = [];
+  
+  // Time range selection default
+  TimeRange _selectedRange = TimeRange.day;
+  String _selectedSensor = 'temperature';
+  
+  // Computed statistics
+  Map<String, SensorStats> _statistics = {};
+
+  // Getters to expose state to the UI
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+  
+  List<DataPoint> get temperatureData => _temperatureData;
+  List<DataPoint> get phData => _phData;
+  List<DataPoint> get waterLevelData => _waterLevelData;
+  List<DataPoint> get lightIntensityData => _lightIntensityData;
+  List<DataPoint> get tdsData => _tdsData;
+  List<DataPoint> get humidityData => _humidityData;
+  
+  TimeRange get selectedRange => _selectedRange;
+  String get selectedSensor => _selectedSensor;
+  Map<String, SensorStats> get statistics => _statistics;
+
+  // Used for showing dialogs/snackbars
+  void setContext(BuildContext context) {
+    _context = context;
+  }
 }
 
-// Define Data Models
+
+
 class DataPoint {
   final DateTime timestamp;
   final double value;
